afa0577c54cf18900eec83688e810bb8be673ea8
Bhagampriyal <106996390+Bhagampriyal@users.noreply.github.com>
2023-07-15 13:58:03 +0530

    Time: 170 ms (18.81%), Space: 7 MB (24.76%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.c
@@ -0,0 +1,24 @@
+int singleNumber(int* nums, int numsSize){
+
+    int a=0,count=0;
+for(int i=0;i<=numsSize;i++)
+{
+    a=0,count=0;
+    for(int j=0;j<numsSize;j++)
+    {
+        if(i!=j && nums[i]==nums[j])
+        {
+            count++;
+            a=count;
+            break;
+            }
+        
+        
+    }
+    if(a==0)
+        {
+            return nums[i];
+        }
+}
+return 0;
+}
